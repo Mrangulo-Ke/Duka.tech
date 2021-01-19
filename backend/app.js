47751ed65615dc953db2dc5c5bb47f0d0698ab49
Mwangekason <mwangekapatrick1@gmail.com>
2021-01-19 09:27:18 +0300

    adding product routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+//importing routes
+const products = require('./routes/products')
+
+app.use('/api/v1', products)
 
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require('./app')
-
+const connnectDatbase = require('./config/database')
 const dotenv = require('dotenv')
 
 //setting up config file
 dotenv.config({ path: 'backend/config/config.env'})
+
+// Connecting to database
+connnectDatbase()
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
